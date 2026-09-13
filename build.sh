cff6220000577832845e4eba8db3a99b5c695e74
sandeepkumar534 <sandeepsamal416@gmail.com>
2026-09-13 20:09:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ set -o errexit
 
 python -m pip install --upgrade pip
 
-python -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-
 python -m pip install -r requirements.txt
+
+python -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
 
 python manage.py migrate
 python manage.py collectstatic --no-input
